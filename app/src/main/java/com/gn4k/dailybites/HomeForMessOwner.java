dbb6dee169c43b0d2267961bafef3e84f2b03c8c
GN4k <2021bcs041@gmail.com>
2023-05-29 02:04:54 +0530

    Started Working on plan editor activity

--- app/src/main/java/com/gn4k/dailybites/HomeForMessOwner.java
+++ app/src/main/java/com/gn4k/dailybites/HomeForMessOwner.java
@@ -2,17 +2,54 @@ package com.gn4k.dailybites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class HomeForMessOwner extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_for_mess_owner);
+        Button silver, gold, diamond;
+        silver=findViewById(R.id.silver);
+        gold=findViewById(R.id.gold);
+        diamond=findViewById(R.id.diamond);
 
+
+        silver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeForMessOwner.this, MonthlyPlanEditor.class);
+                intent.putExtra("planName", "Silver");
+                startActivity(intent);
+            }
+        });
+
+        gold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeForMessOwner.this, MonthlyPlanEditor.class);
+                intent.putExtra("planName", "Gold");
+                startActivity(intent);
+            }
+        });
+
+        diamond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeForMessOwner.this, MonthlyPlanEditor.class);
+                intent.putExtra("planName", "Diamond");
+                startActivity(intent);
+            }
+        });
 
 
 
--- app/src/main/java/com/gn4k/dailybites/MonthlyPlanEditor.java
+++ app/src/main/java/com/gn4k/dailybites/MonthlyPlanEditor.java
@@ -2,13 +2,50 @@ package com.gn4k.dailybites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MonthlyPlanEditor extends AppCompatActivity {
 
+    TextView TVplanName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_plan_editor);
+
+        TVplanName = findViewById(R.id.tv_planName);
+
+        Bundle bundle = getIntent().getExtras();
+        String planName = bundle.getString("planName");
+
+        setAllThingsAccordingToPlan(planName);
+
+
+
+
+    }
+
+    private void setAllThingsAccordingToPlan(String planName) {
+
+
+        TVplanName.setText(planName + " Plan");
+
+        if(planName.equals("Diamond")){
+
+            TVplanName.setTextColor(Color.parseColor("#9747FF"));
+
+        }
+        else if (planName.equals("Gold")) {
+
+            TVplanName.setTextColor(Color.parseColor("#FDD500"));
+
+        }
+        else if (planName.equals("Silver")) {
+
+            TVplanName.setTextColor(Color.parseColor("#B5B5B5"));
+
+        }
+
     }
 }

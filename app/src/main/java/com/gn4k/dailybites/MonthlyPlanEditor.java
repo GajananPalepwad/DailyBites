f5203b2cafe58132b38a2bda6ec77ee40d76cfaa
GN4k <2021bcs041@gmail.com>
2023-05-29 13:32:04 +0530

    Completed the monthly plan editor with all design logic

--- app/src/main/java/com/gn4k/dailybites/MonthlyPlanEditor.java
+++ app/src/main/java/com/gn4k/dailybites/MonthlyPlanEditor.java
@@ -3,18 +3,22 @@ package com.gn4k.dailybites;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MonthlyPlanEditor extends AppCompatActivity {
 
     TextView TVplanName;
+    ImageView planImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_plan_editor);
 
         TVplanName = findViewById(R.id.tv_planName);
+        planImage = findViewById(R.id.planImage);
 
         Bundle bundle = getIntent().getExtras();
         String planName = bundle.getString("planName");
@@ -34,16 +38,23 @@ public class MonthlyPlanEditor extends AppCompatActivity {
         if(planName.equals("Diamond")){
 
             TVplanName.setTextColor(Color.parseColor("#9747FF"));
+            Drawable drawable  = getResources().getDrawable(R.drawable.diamond);
+            planImage.setImageDrawable(drawable);
+
 
         }
         else if (planName.equals("Gold")) {
 
             TVplanName.setTextColor(Color.parseColor("#FDD500"));
+            Drawable drawable  = getResources().getDrawable(R.drawable.gold);
+            planImage.setImageDrawable(drawable);
 
         }
         else if (planName.equals("Silver")) {
 
             TVplanName.setTextColor(Color.parseColor("#B5B5B5"));
+            Drawable drawable  = getResources().getDrawable(R.drawable.silver);
+            planImage.setImageDrawable(drawable);
 
         }
 

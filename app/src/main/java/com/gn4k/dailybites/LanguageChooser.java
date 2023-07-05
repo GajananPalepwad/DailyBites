e2f9e4f967739579fed3a72331285ccd9f5ab896
GN4k <2021bcs041@gmail.com>
2023-07-06 02:26:52 +0530

    hindi Language is done

--- app/src/main/java/com/gn4k/dailybites/LanguageChooser.java
+++ app/src/main/java/com/gn4k/dailybites/LanguageChooser.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+
+import com.airbnb.lottie.L;
+
+import java.util.Locale;
 
 public class LanguageChooser extends AppCompatActivity {
 
@@ -16,21 +21,44 @@ public class LanguageChooser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language_chooser);
-        eng=findViewById(R.id.english);
+        eng = findViewById(R.id.english);
+        hindi = findViewById(R.id.hindi);
 
-        eng.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        eng.setOnClickListener(v -> {
 
-                if(getIntent().getStringExtra("data")!= null) {
-                    onBackPressed();
-                }else if(getIntent().getStringExtra("data") == null){
-                    Intent intent = new Intent(LanguageChooser.this, ChooseMessOrUser.class);
-                    startActivity(intent);
-                }
+            if(getIntent().getStringExtra("data")!= null) {
+                onBackPressed();
+            }else if(getIntent().getStringExtra("data") == null){
+                Intent intent = new Intent(LanguageChooser.this, ChooseMessOrUser.class);
+                startActivity(intent);
+            }
+        });
+
+        hindi.setOnClickListener(v -> {
+
+            if(getIntent().getStringExtra("data")!= null) {
+                setLanguage("hi");
+                onBackPressed();
+            }else if(getIntent().getStringExtra("data") == null){
+                setLanguage("hi");
+                Intent intent = new Intent(LanguageChooser.this, ChooseMessOrUser.class);
+                startActivity(intent);
             }
         });
 
 
     }
+
+    private void setLanguage(String language){
+
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+
+    }
+
+
 }
